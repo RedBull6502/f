78e8a75a94799daa59a592dc352e460f72ab7d91
RedBull6502 <157056503+RedBull6502@users.noreply.github.com>
2024-01-29 14:10:08 +0300

    Update Komissarova.cs

--- Komissarova.cs
+++ Komissarova.cs
@@ -28,9 +28,24 @@ namespace Komissarova_Task1
             Console.WriteLine("Ответ:" + z2);
             Console.ReadKey();
             ///3
-            double x3 = -2.34;
-            double h = Abs(x3 - 5) - 
-
+            double x = -2.34;
+			double d = 2;
+			double y = (Math.Abs(x - 5) - Math.Sin(x))/3 + Math.Sqrt(Math.Pow(x, d)+ 2014)*Math.Cos(2*x)-3;
+			Console.WriteLine("Ответ:" + Math.Round(y, 5));
+///4
+          double l1 = 3.6;
+		   double k1 = 4;
+		   double j1 = Math.Exp(l1-2) + Math.Abs(Math.Sin(l1)) - Math.Pow(l1, k1) * Math.Cos(1/l1);
+		   Console.WriteLine("Ответ:" + Math.Round(j1, 4));
+            ///5
+             	 	double b = 2 ;
+		double l2 = 0.1;
+		double k2 = 0.2;
+		double g2 = 1;
+		double s2 = 5;
+			double s3 =2;
+		double j2 =Math.Pow(Math.Sqrt(Math.Pow(g2,b)+k2),s2) - (Math.Pow(k2, b) * Math.Pow((Math.Sin(l2 + g2)),3))/g2;
+		Console.WriteLine("Ответ:" +  Math.Round(j2, 4));
 
 
         }
