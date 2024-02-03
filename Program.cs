81fc32bd603c0dbc6d82447ca6b566790feb6ec4
RedBull6502 <157056503+RedBull6502@users.noreply.github.com>
2024-02-03 12:06:27 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -31,6 +31,8 @@ namespace УСЛОВНЫЕ_ОПЕРАТОРЫ_Комиссарова
                 Console.ReadKey();
             }
 
+
+            
             //Задача 2
             Console.WriteLine("Введите три числа:");
             int a1 = Convert.ToInt32(Console.ReadLine());
@@ -57,6 +59,104 @@ namespace УСЛОВНЫЕ_ОПЕРАТОРЫ_Комиссарова
                 Console.WriteLine("Есть равные числа");
                 Console.ReadKey();
             }
+
+            
+             ///Задача 3
+            Console.WriteLine("Введите три числа:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+
+           double sum1 = a + b;
+            double sum2 = b + c;
+            double sum3 = c + a;
+
+            double max_sum = Math.Max(sum1, Math.Max(sum2, sum3));
+           
+
+            if (max_sum == sum1)
+            {
+                Console.WriteLine($"Наибольшая сумма : {a}+{b}={sum1}");
+            }
+            else if (max_sum == sum1)
+            {
+                Console.WriteLine($"Наибольшая сумма : {a}+{c}={sum2}");
+            }
+            if (max_sum == sum2)
+            {
+                Console.WriteLine($"Наибольшая сумма : {b}+{c}={sum2}");
+            }
+             else if (max_sum == sum2)
+            {
+                Console.WriteLine($"Наибольшая сумма : {a}+{c}={sum3}");
+            }
+
+            
+            ///задача 4
+              Console.WriteLine("Введите высоту кирпича:");
+            double height = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ширину кирпича:");
+            double width = Convert.ToDouble(Console.ReadLine());
+            
+            if ((height <= 50 && width <=
+                120)|| (height<= 120 && width <= 50))
+            {
+                Console.WriteLine("Кирпич поместится");
+            }
+            else
+            {
+                Console.WriteLine("Кирпич не поместится");
+            }
+
+             ///задача 5
+            Console.WriteLine("Введите номер дня недели(1-Понедельник, 2-Вторник и т.д.):");
+            int number = Convert.ToInt32(Console.ReadLine());
+            string name =GetName(number);
+            if (name != null)
+            {
+                Console.WriteLine($"День недели под номером {number} - это {name}");
+            }
+            else
+                {
+                    Console.WriteLine("Некорректный номер дня недели");
+                }
+            static string GetName (int number)
+            {
+                switch (number)
+                {
+                    case 1:
+                        return "Понедельник";
+                    case 2:
+                        return "Вторник";
+                    case 3:
+                        return "Среда";
+                    case 4:
+                        return "Четверг";
+                    case 5:
+                        return "Пятница";
+                    case 6:
+                        return "Суббота";
+                    case 7:
+                        return "Воскресенье";
+                    default:
+                        return null;
+///Задача 5
+            Console.WriteLine("Введите коэффициенты квадратного уравнения ax ^ 2 + bx + c = 0:");
+            Console.Write(" a = ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write(" b = ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write(" c = ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            double discriminant = b * b - 4 * a * c;
+            if (discriminant > 0)
+            {
+                double x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                double x2 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                }
         }
     }
 }
